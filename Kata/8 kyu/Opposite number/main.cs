b6ccbbc72657eefa7b697698246984b0abc059e2
Zippocat <zipocat@gmail.com>
2019-05-21 04:23:56 +0300

    Opposite number
    https://www.codewars.com/kata/56dec885c54a926dcd001095

---
+++ Kata/8 kyu/Opposite number/main.cs
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+// Opposite number
+
+namespace Opposite_number
+{
+    public class Kata
+    {
+        public static int Opposite(int number)
+        {
+            return number * -1;
+        }
+    }
+}

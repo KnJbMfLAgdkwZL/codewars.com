3f8f9ddad31012317fb916c22028880b7245b56c
Zippocat <zipocat@gmail.com>
2019-05-26 20:32:35 +0300

    Unlucky Days https://www.codewars.com/kata/56eb0be52caf798c630013c0

---
+++ Kata/7 kyu/Unlucky Days/main.js
@@ -0,0 +1,13 @@
+// Unlucky Days
+// https://www.codewars.com/kata/56eb0be52caf798c630013c0
+
+function unluckyDays(year) {
+    let count = 0;
+    for (let i = 0; i < 12; i++) {
+        let date = new Date(year, i, 13, 12, 00);
+        if (date.getDay() === 5) {
+            count++;
+        }
+    }
+    return count;
+}
